bde7cf7474acea166095e132aaa8d8a3e328c19c
frinsila <frinsilasherin@gmail.com>
2026-06-10 22:25:30 +0400

    updated

--- app.py
+++ app.py
@@ -12,9 +12,25 @@ def home():
 
 @app.route("/price")
 def price():
-    data = requests.get(BINANCE_URL).json()
-    return jsonify({"price": float(data["price"])})
+    try:
+        response = requests.get(
+            "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
+            timeout=10
+        )
 
+        print("Status:", response.status_code)
+        print("Response:", response.text)
+
+        data = response.json()
+
+        return jsonify({
+            "price": float(data.get("price", 0))
+        })
+
+    except Exception as e:
+        print("ERROR:", str(e))
+        return jsonify({"error": str(e)}), 500
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
