18c9c623dc7298037460c0b77d7a619c98607045
frinsila <frinsilasherin@gmail.com>
2026-06-11 12:47:26 +0400

    updated

--- app.py
+++ app.py
@@ -1,38 +1,10 @@
-from flask import Flask, render_template, jsonify
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-COINGECKO_URL = (
-    "https://api.coingecko.com/api/v3/simple/price"
-    "?ids=bitcoin&vs_currencies=usd"
-    "&include_24hr_change=true"
-)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/price")
-def price():
-    try:
-        response = requests.get(
-            "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true",
-            timeout=10
-        )
-
-        print("Status Code:", response.status_code)
-        print("Response:", response.text)
-
-        data = response.json()
-
-        return jsonify({
-            "price": data["bitcoin"]["usd"],
-            "change_24h": data["bitcoin"]["usd_24h_change"]
-        })
-
-    except Exception as e:
-        print("ERROR:", str(e))
-        return jsonify({"error": str(e)}), 500
 if __name__ == "__main__":
     app.run()
