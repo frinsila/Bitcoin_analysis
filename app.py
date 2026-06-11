1a5926ac6038e212bc220b86593da551d4d56f6f
frinsila <frinsilasherin@gmail.com>
2026-06-11 12:37:40 +0400

    updated

--- app.py
+++ app.py
@@ -16,20 +16,23 @@ def home():
 @app.route("/price")
 def price():
     try:
-        response = requests.get(COINGECKO_URL, timeout=10)
-        response.raise_for_status()
+        response = requests.get(
+            "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true",
+            timeout=10
+        )
+
+        print("Status Code:", response.status_code)
+        print("Response:", response.text)
 
         data = response.json()
 
         return jsonify({
             "price": data["bitcoin"]["usd"],
-            "change_24h": round(data["bitcoin"]["usd_24h_change"], 2)
+            "change_24h": data["bitcoin"]["usd_24h_change"]
         })
 
     except Exception as e:
-        return jsonify({
-            "error": str(e)
-        }), 500
-
+        print("ERROR:", str(e))
+        return jsonify({"error": str(e)}), 500
 if __name__ == "__main__":
     app.run()
